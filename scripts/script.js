afbf4a8a1ff3e31da6897110576fd1317643461c
Колпакова Валерия Александровна <VAlKolpakova@sberbank.ru>
2021-03-26 02:13:25 +0300

    refactor: fix class name, БЭМ

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 let likesContainer = document.querySelector('.elements');
 let likes = likesContainer.querySelectorAll('.elements__card-like');
-let formContainer = document.querySelector('.form__dark-background');
+let formContainer = document.querySelector('.form-wrapper');
 let form = formContainer.querySelector('.form');
 let root = document.querySelector('.root');
 let profileContainer = document.querySelector('.profile');
-let inputName = form.querySelector('.form-enable__input-name');
-let inputDescriptions = form.querySelector('.form-enable__input-descriptions');
+let inputName = form.querySelector('.form__input-name');
+let inputDescriptions = form.querySelector('.form__input-descriptions');
 let profileName = profileContainer.querySelector('.profile__name');
 let profileDescriptions = profileContainer.querySelector('.profile__description');
 console.log(likes);
@@ -27,8 +27,8 @@ let editBtn = document.querySelector('.profile__edit-button');
 function openForm() {
    form.classList.toggle('form');
    form.classList.toggle('form-enable');
-   formContainer.classList.toggle('form__dark-background');
-   formContainer.classList.toggle('form-enable__dark-background');
+   formContainer.classList.toggle('form-wrapper');
+   formContainer.classList.toggle('form__dark');
 
    root.setAttribute('style', 'overflow: hidden');
 
@@ -39,14 +39,14 @@ function openForm() {
 editBtn.addEventListener('click', openForm);
 
 
-let saveBtn = document.querySelector('.form-enable__save-btn');
+let saveBtn = document.querySelector('.form__save-btn');
 
 function saveChanges() {
    profileName.textContent = inputName.value;
    profileDescriptions.textContent = inputDescriptions.value;
 
-   formContainer.classList.toggle('form__dark-background');
-   formContainer.classList.toggle('form-enable__dark-background');
+   formContainer.classList.toggle('form-wrapper');
+   formContainer.classList.toggle('form__dark');
    form.classList.remove('form-enable');
    form.classList.add('form');
    
@@ -56,14 +56,14 @@ function saveChanges() {
 saveBtn.addEventListener('click', saveChanges);
 
 
-let closeBtn = document.querySelector('.form-enable__hidden-btn');
+let closeBtn = document.querySelector('.form__hidden-btn');
 
 function closeForm() {
    profileName.textContent = profileName.textContent;
    profileDescriptions.textContent = profileDescriptions.textContent;
    
-   formContainer.classList.toggle('form__dark-background');
-   formContainer.classList.toggle('form-enable__dark-background');
+   formContainer.classList.toggle('form-wrapper');
+   formContainer.classList.toggle('form__dark');
    form.classList.remove('form-enable');
    form.classList.add('form');
 
