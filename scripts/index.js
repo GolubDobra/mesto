b00b6ad05bb369ccc9b8e853a0d52b5c007da2a7
Колпакова Валерия Александровна <VAlKolpakova@sberbank.ru>
2021-04-03 01:02:55 +0300

    Style: edit style's files

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,17 @@
 let popupContainer = document.querySelector('.popup');
 let popup = popupContainer.querySelector('.popup__container');
+let popupForm = popup.querySelector('.popup__form');
 
 let root = document.querySelector('.root');
 let profileContainer = document.querySelector('.profile');
 let profileDescriptions = profileContainer.querySelector('.profile__description');
 let profileName = profileContainer.querySelector('.profile__name');
 
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputDescriptions = popup.querySelector('.popup__input_type_descriptions');
+let inputName = popupForm.querySelector('.popup__input_type_name');
+let inputDescriptions = popupForm.querySelector('.popup__input_type_descriptions');
 
 
-popup.addEventListener('submit', function (evt) {
+popupForm.addEventListener('submit', function (evt) {
    evt.preventDefault();
    profileName.textContent = inputName.value;
    profileDescriptions.textContent = inputDescriptions.value;
